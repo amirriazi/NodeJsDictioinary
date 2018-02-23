c7bd4a712b4365177b35b94107afb43a255882b3
amirriazi <mohammad@amirriazi.com>
2018-02-23 12:22:16 +0330

    remove some remarks!

--- public/projectValues.js
+++ public/projectValues.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-     server_port: process.env.PORT || 3000, // for publishing in openshift
-     server_ip_address: process.env.IP || '127.0.0.1' // for publishing in openshift
+     server_port: process.env.PORT || 3000, 
+     server_ip_address: process.env.IP || '127.0.0.1' 
 
 };
 
